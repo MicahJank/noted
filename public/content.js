65e3716255d222295d81cd0b92dff02a8adc47d7
MicahJank <MicahJank@users.noreply.github.com>
2021-04-27 23:12:14 -0500

    making a little headway on the crop window that needs to appear when the user is selecting an area of the window to screenshot

--- public/content.js
+++ public/content.js
@@ -7,22 +7,38 @@ function run(request, sender, sendResponse) {
         shadowContainer.id = 'extension--shadow-container';
         let overlay = document.createElement('div');
         overlay.id = 'crop-overlay';
+        overlay.className = 'overlay';
         let shadow = shadowContainer.attachShadow({ mode: 'open' });
         let style = document.createElement('style');
 
+        let testContainer = document.createElement('div');
+        testContainer.className = 'test';
+
         style.textContent = `
-            div {
+            .overlay {
                 position: absolute;
                 top: 0;
-                background-color: RGBA(0,0,0,0.59);
+                background-color: RGBA(0,0,0);
                 width: 100vw;
                 height: 100vh;
                 cursor: crosshair;
+                filter: opacity(0.2);
+            }
+
+            .test {
+                position: absolute;
+                top: 30%;
+                left: 30%;
+                width: 300px;
+                height: 200px;
+                filter: opacity(1);
+                background-color: #fff;
             }
         `
         shadowContainer.appendChild(shadow);
         shadow.appendChild(style);
         shadow.appendChild(overlay);
+        overlay.appendChild(testContainer);
         document.body.appendChild(shadowContainer);
     }
     // return true;
